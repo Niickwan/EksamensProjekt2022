6f4ea56f1915dc137f99e2c4b9fdce68b059d9ef
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2022-05-12 10:30:43 +0200

    - Created and fixed the FragmentLoginHome and made connection with the database + FragmentLoginHome now lets the user login with the given UserRights according to the database, via the connection established in DB_Con.

---
+++ app/src/main/java/com/jmmnt/Database/DB_Con.java
@@ -0,0 +1,55 @@
+package com.jmmnt.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DB_Con {
+    private Connection connection;
+    private PreparedStatement preStmt;
+    private Statement stmt;
+    private ResultSet rs;
+    private static DB_Con dbCon;
+    private final String URL = "jdbc:mysql://mysql61.unoeuro.com:3306/dat32_dk_db_eksamen";
+    private final String format = "yyyy-MM-dd";
+
+    private Connection connection() {
+        connection = null;
+        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, "dat32_dk", "9hkdpBFtAg34");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static DB_Con getInstance() {
+        if (dbCon == null) {
+            System.out.println("DB_Con Created");
+            return dbCon = new DB_Con();
+        } else
+            return dbCon;
+    }
+
+    public int validateLogin(String username, String password) throws SQLException {
+        int userRights = -1;
+        String MySQL = "SELECT * FROM User WHERE Username = ? AND Password = ?";
+        connection = connection();
+        preStmt = connection.prepareStatement(MySQL);
+        preStmt.setString(1, username);
+        preStmt.setString(2, password);
+        rs = preStmt.executeQuery();
+        if (rs.next()) {
+            if (rs.getString("UserRights").equals("1")) userRights = 1;
+            else if (rs.getString("UserRights").equals("2")) userRights = 2;
+            connection.close();
+            preStmt.close();
+            rs.close();
+        }
+        return userRights;
+    }
+}
