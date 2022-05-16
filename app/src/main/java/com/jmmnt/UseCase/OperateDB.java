2fcc66ab833491e3434d71706dbec0c28fcc0987
GodeGamleJacob <89967283+GodeGamleJacob@users.noreply.github.com>
2022-05-16 11:57:50 +0200

    - Created the method isEmailAvailable in OperateDB.java - Created the method isEmailAvailable in DB_Con.java + Made it so that the method will check if the email is in use in the database, where it will return false, if such is the case.

--- app/src/main/java/com/jmmnt/UseCase/OperateDB.java
+++ app/src/main/java/com/jmmnt/UseCase/OperateDB.java
@@ -16,4 +16,8 @@ public class OperateDB {
         return db_con.validateLogin(email, password);
     }
 
+    public boolean isEmailAvailable(String email) throws SQLException {
+        return db_con.isEmailAvailable(email);
+    }
+
 }
