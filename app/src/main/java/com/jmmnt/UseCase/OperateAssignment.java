5751cc3dd97a381044f67d823f3ee972105c8b5a
fisk810 <marc571m@edu.easj.dk>
2022-05-19 19:48:37 +0200

    Marcus
    +In OperateAssignment.java the search methods getSearchedObjectsMultipleStrings and getSearchedObjects now has the ability to search for objects with specific statuses.

--- app/src/main/java/com/jmmnt/UseCase/OperateAssignment.java
+++ app/src/main/java/com/jmmnt/UseCase/OperateAssignment.java
@@ -12,10 +12,11 @@ public class OperateAssignment {
     //TODO søge KUN på aktive eller KUN på ikke aktive
     //SEARCHING METHODS------------------------------------------------------
 
-    public List<Object> getSearchedObjectsMultipleStrings(List<Object> objects, String[] objectStrings, String input){
+    //ObjectStatus should be null if the searched objects dont need a specific status.
+    public List<Object> getSearchedObjectsMultipleStrings(List<Object> objects, String[] objectStrings, String input, String objectStatus){
         ArrayList<Object> multipleStringSearchArray = new ArrayList<>();
         for (String objectString : objectStrings) {
-            multipleStringSearchArray.addAll(getSearchedObjects(objects, objectString, input));
+            multipleStringSearchArray.addAll(getSearchedObjects(objects, objectString, input, objectStatus));
         }
         return multipleStringSearchArray;
     }
@@ -38,9 +39,15 @@ public class OperateAssignment {
         }
         return matchingCasesIndex;
     }
-
-    public List<Object> getSearchedObjects(List<Object> objects, String objectString, String input){
-        return sortObjectsByindex(objects, findObjectsMatchingInput(objects,objectString, input));
+    //ObjectStatus should be null if the searched objects dont need a specific status.
+    public List<Object> getSearchedObjects(List<Object> objects, String objectString, String input, String objectStatus){
+        if(objectStatus == null) {
+            return sortObjectsByindex(objects, findObjectsMatchingInput(objects, objectString, input));
+        }
+        else{
+            objects = sortObjectsByindex(objects, findObjectsMatchingInput(objects, objectStatus, input));
+            return sortObjectsByindex(objects, findObjectsMatchingInput(objects, objectString, input));
+        }
     }
 
 
